65633c714d2d54c5bfc881703e22140b8ae7828c
Duong <trananh123duong@gmail.com>
2023-09-15 10:34:48 +0700

    setting database

--- yii/config/db.php
+++ yii/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db;dbname=app', // Sử dụng tên dịch vụ 'db' (tên container của MySQL trong docker-compose.yml)
+    'username' => 'devs',
+    'password' => 'devs123!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
